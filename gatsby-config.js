fd658e9f038440305b04c6932a759b0d00db0e4c
GraysonNull <graysonnull@me.com>
2020-04-01 20:59:29 +0000

    remove trailing slash package, fix paths in json file to with with trailing slashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify`,
-    `gatsby-plugin-remove-trailing-slashes`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
