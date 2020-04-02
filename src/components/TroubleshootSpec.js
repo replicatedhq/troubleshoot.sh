f071eafe376500181e70d753cc3a4f30321f9aad
jelena <jelena.gruica@gmail.com>
2020-04-02 11:08:48 -0700

    Bug fixes on spec detail page

--- src/components/TroubleshootSpec.js
+++ src/components/TroubleshootSpec.js
@@ -108,7 +108,10 @@ class TroubleshootSpec extends React.Component {
       <div className="u-width--full u-overflow--auto flex-column flex1">
         <div className="section landing-header">
           <div className={`${!isMobile ? "flex1" : "flex-column"} container flex justifyContent--center`}>
-            <div className="section">
+            <div className={`${!isMobile && "troubleshootSection troubleshootSectionWidth"}`}>
+              <Link to="/explore" className="flex flex1 u-marginBottom--15 u-fontSize--small link">
+                <span className="icon backArrow-icon u-marginRight--small" /> Find more specs
+                </Link>
               <p className="u-fontSize--largest u-color--biscay u-lineHeight--more u-fontWeight--medium"> {currentSpec?.title} </p>
               <p className="u-fontSize--large u-color--dustyGray u-lineHeight--normal u-marginBottom--20 u-marginTop--small body-copy"> {currentSpec?.description} </p>
               <div className="MonacoEditor--wrapper flex u-width--full">
@@ -121,7 +124,7 @@ class TroubleshootSpec extends React.Component {
                   : <span className="u-color--vidaLoca u-marginRight--30">{copySuccess}</span>}
               </div>
             </div>
-            <div className={`section ${!isMobile && "u-marginLeft--50"}`}>
+            <div className={`${!isMobile ? "troubleshootSection u-marginLeft--50 troubleshootSectionWidth" : "u-marginTop--30"}`}>
               <div className="flex alignItems--center">
               <p className="u-fontSize--large u-color--biscay u-lineHeight--more u-fontWeight--medium"> Contributors </p>
               {currentSpec?.contributors?.map((contributor, i) => (
