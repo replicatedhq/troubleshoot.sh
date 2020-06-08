941a8f5024ac1b4fe29f443989e92fbcc4578ce2
jelena <jelena.gruica@gmail.com>
2020-06-08 11:50:52 -0700

    Documentation style updates

---
+++ gatsby-theme-troubleshoot/src/gatsby-theme-apollo-docs/components/page-content.js
@@ -0,0 +1,95 @@
+import PropTypes from "prop-types";
+import React, { useRef } from "react";
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div({
+  display: "flex",
+  alignItems: "flex-start"
+});
+
+const InnerWrapper = styled.div({
+  flexGrow: 1,
+  width: 0
+});
+
+const BodyContent = styled.div({
+  fontFamily: "Helvetica Neue",
+  fontStyle: "normal",
+  fontWeight: "normal",
+  color: "#4A4A4A",
+  [["h1", "h2", "h3", "h4", "h5", "h6"]]: {
+    code: {
+      whiteSpace: "normal"
+    },
+    a: {
+      color: "inherit",
+      textDecoration: "none",
+      "&.anchor": {
+        display: "none"
+      }
+    },
+    color: "#163166",
+    fontFamily: "Roboto Mono",
+    fontStyle: "normal",
+    fontWeight: "bold",
+  },
+  "*:not(style) +": {
+    [["h1", "h2", "h3"]]: {
+      marginBottom: "20px"
+    },
+    [["h4", "h5", "h6"]]: {
+      marginBottom: "15px"
+    }
+  },
+  "p": {
+    fontSize: "16px",
+    lineHeight: "28px",
+  },
+  "h2": {
+    fontSize: "24px",
+    lineHeight: "34px"
+  },
+  "h3": {
+    fontSize: "22px",
+    lineHeight: "32px"
+  },
+  "h4": {
+    fontSize: "20px",
+    lineHeight: "30px"
+  },
+  "h5": {
+    fontSize: "18px",
+    lineHeight: "28px"
+  },
+  "h3": {
+    fontSize: "16px",
+    lineHeight: "26px"
+  }
+});
+
+
+export default function PageContent(props) {
+  const contentRef = useRef(null);
+
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        <BodyContent ref={contentRef} className="content-wrapper">
+          {props.children}
+        </BodyContent>
+      </InnerWrapper>
+    </Wrapper>
+  );
+}
+
+PageContent.propTypes = {
+  children: PropTypes.node.isRequired,
+  pathname: PropTypes.string.isRequired,
+  githubUrl: PropTypes.string,
+  pages: PropTypes.array.isRequired,
+  hash: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  graphManagerUrl: PropTypes.string.isRequired,
+  headings: PropTypes.array.isRequired,
+  spectrumUrl: PropTypes.string
+};
