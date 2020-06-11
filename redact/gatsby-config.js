5dfc9cedd3443303c72d67bca9b22f2e1ef330de
jelena <jelena.gruica@gmail.com>
2020-06-11 13:43:23 -0700

    Fixing console errors

--- redact/gatsby-config.js
+++ redact/gatsby-config.js
@@ -1,33 +1,33 @@
 const themeOptions = {
-  siteName: 'Troubleshoot Docs',
-  pageTitle: 'Troubleshoot Docs',
-  menuTitle: 'Troubleshoot',
-  baseDir: 'redact',
-  contentDir: 'source',
+  siteName: "Troubleshoot Docs",
+  pageTitle: "Troubleshoot Docs",
+  menuTitle: "Troubleshoot",
+  baseDir: "redact",
+  contentDir: "source",
 };
 
 module.exports = {
   siteMetadata: {
-    title: "Troubleshoot redactors"
+    title: "Troubleshoot Redactors"
   },
-  pathPrefix: '/redact',
+  pathPrefix: "/redact",
   plugins: [
     {
-      resolve: '../gatsby-theme-troubleshoot',
+      resolve: "../gatsby-theme-troubleshoot",
       options: {
         ...themeOptions,
         root: __dirname,
-        subtitle: 'Troubleshoot readactors',
-        description: 'The Official Troubleshoot Documentation',
-        githubRepo: 'replicatedhq/troubleshoot',
+        subtitle: "Troubleshoot readactors",
+        description: "The Official Troubleshoot Documentation",
+        githubRepo: "replicatedhq/troubleshoot",
         sidebarCategories: {
           null: [
-            'index',
-            'reference',
+            "index",
+            "reference",
           ],
           Redactors: [
-            'database-connection-strings',
-            'aws-credentials',
+            "database-connection-strings",
+            "aws-credentials",
           ]
         },
       },
