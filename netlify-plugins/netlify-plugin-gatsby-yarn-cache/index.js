dd445a0543c16e69bda5babe5b3f117fa3cc39f2
Mark Pundsack <markpundsack@users.noreply.github.com>
2020-09-21 17:03:34 -0500

    Move cache save and restore to plugin

---
+++ netlify-plugins/netlify-plugin-gatsby-yarn-cache/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+// Should fetch workspaces like `jq -r '.workspaces|join(" ")' < package.json`
+
+const getCacheDirs = (constants) => [
+  constants.PUBLISH_DIR,
+  path.normalize(`${constants.PUBLISH_DIR}/../.cache`),
+  './marketing/.cache',
+  './marketing/public',
+  './docs/.cache',
+  './docs/public',
+];
+
+module.exports = {
+  async onPreBuild({ constants, utils }) {
+    // print a helpful message if the publish dir is misconfigured
+    if (process.cwd() === constants.PUBLISH_DIR) {
+      utils.build.failBuild(
+        `Gatsby sites must publish the public directory, but your site’s publish directory is set to “${constants.PUBLISH_DIR}”. Please set your publish directory to your Gatsby site’s public directory.`,
+      );
+    }
+
+    const cacheDirs = getCacheDirs(constants);
+
+    if (await utils.cache.restore(cacheDirs)) {
+      console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
+    } else {
+      console.log('No Gatsby cache found. Building fresh.');
+    }
+  },
+  async onPostBuild({ constants, utils }) {
+    const cacheDirs = getCacheDirs(constants);
+
+    if (await utils.cache.save(cacheDirs)) {
+      console.log('Stored the Gatsby cache to speed up future builds.');
+    } else {
+      console.log('No Gatsby build found.');
+    }
+  },
+};
