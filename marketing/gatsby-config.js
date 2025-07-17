876ec58fcc9ed26597d3afd58ebf7c60861c9d97
Diamon Wiggins <diamonw757@gmail.com>
2025-07-17 17:32:21 -0400

    f

--- marketing/gatsby-config.js
+++ marketing/gatsby-config.js
@@ -3,6 +3,13 @@ module.exports = {
     title: "Troubleshoot.sh"
   },
   pathPrefix: '/',
+  flags: {
+    FAST_DEV: false,
+    DEV_SSR: false,
+    PRESERVE_WEBPACK_CACHE: false,
+    PRESERVE_FILE_DOWNLOAD_CACHE: false,
+    PARALLEL_SOURCING: false
+  },
   plugins: [
     {
       resolve: '../gatsby-theme-marketing',
