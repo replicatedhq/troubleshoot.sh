0135873f58808268122f622dab4def73a6f86d23
Diamon Wiggins <38189728+diamonwiggins@users.noreply.github.com>
2025-07-22 11:49:25 -0400

    chore: restore old favicon and social card  (#635)
    * restore old favicon
    
    * update favicon and social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/troubleshoot-social-card.svg',
     colorMode: {
       defaultMode: 'light',
       disableSwitch: true,
